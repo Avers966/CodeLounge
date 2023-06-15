e13ab7004ca7d4edef308bca35f1d920b912d4d5
Grigory87 <dyakonoff.grigory@yandex.ru>
2023-06-15 14:22:06 +0300

    added 2 annotation EventNotificationDto

--- src/main/java/ru/skillbox/diplom/group35/library/core/dto/streaming/EventNotificationDto.java
+++ src/main/java/ru/skillbox/diplom/group35/library/core/dto/streaming/EventNotificationDto.java
@@ -2,6 +2,8 @@ package ru.skillbox.diplom.group35.library.core.dto.streaming;
 
 import java.util.UUID;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 /**
  * EventNotification
@@ -9,6 +11,8 @@ import lombok.Data;
  * @author Marat Safagareev
  */
 @Data
+@NoArgsConstructor
+@Accessors(chain = true)
 public class EventNotificationDto {
 
   private UUID authorId;
